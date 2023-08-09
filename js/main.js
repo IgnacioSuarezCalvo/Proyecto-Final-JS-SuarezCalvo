f4091c6b7af035d99f7703d930e8ee5525b70384
Ignacio <igf.suarez@gmail.com>
2023-08-09 13:14:42 -0300

    js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,67 @@
 const listaNotas = document.getElementById('lista-notas');
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit' , e =>{
+     e.preventDefault();
+
+     checkInputs();
+});
+
+function checkInputs(){
+     const usernameValue = username.value.trim();
+     const emailValue = email.value.trim();
+     const passwordValue = password.value.trim();
+     const password2Value = password2.value.trim();
+
+     if(username === ''){
+          setErrorFor(username, 'Completar');
+     }else {
+               setSuccessFor(username);
+          }
+
+     if(emailname === ''){
+          setErrorFor(email, 'El Email tiene que completarse');
+     } else if (!isEmail(emailValue)) {
+          serErrorFor(email, 'No es valido este email');
+     } else{
+          setSuccessFor(username);
+     }
+       
+     if(passwordValue === ''){
+          setErrorFor(password, 'Completar');
+     }else{
+          setSuccessFor(password);
+     }
+
+     if(password2Value === ''){
+          setErrorFor(password2, 'Completar');
+     } else if(passwordValue !== password2Value){
+          setErrorFor(password2, 'No son iguales');
+     } else{
+          setSuccessFor(password2);
+     }
+}
+
+function setErrorFor(input, message){
+     const formControl = input.parentElement;
+     const small = formControl.querySelector('small');
+     formControl.className = 'form-control error';
+     small.innerText = message;
+}
+
+function setSuccessFor(input){
+     const formControl = input.parentElement;
+     formControl.className = 'form-control success'
+}
+
+function isEmail(email){
+     return email.test(email);
+}
+
 eventListeners();
 
 function eventListeners() {
