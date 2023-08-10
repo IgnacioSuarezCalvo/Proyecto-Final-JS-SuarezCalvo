4365c38a3165a041f29b7538bf33b0f2c51de67d
Ignacio <igf.suarez@gmail.com>
2023-08-10 14:56:06 -0300

    prueba

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const signupForm = document.querySelector('#signupForm')
-const loginForm = document.querySelector('loginForm')
+const loginForm = document.querySelector('#loginForm')
+const logout = document.querySelector('#logout')
+
+
 signupForm.addEventListener('submit',(e)=>{
      e.preventDefault()
      const name = document.querySelector('#name').value
@@ -13,6 +16,7 @@ signupForm.addEventListener('submit',(e)=>{
             icon: 'error',
             title: 'Oops...',
             text: 'Ya esta registrado',
+            timer: 1000
           })
      }
      Users.push({name: name, email: email, password: password})
@@ -21,10 +25,48 @@ signupForm.addEventListener('submit',(e)=>{
         icon: 'success',
         title: 'Felicitaciones',
         text: 'Ya estas registrados',
+        timer: 1000
       })
       window.location.href = '../index.html'
 })
 
+loginForm.addEventListener('submit', (e)=>{
+     e.preventDefault()
+     const Users = JSON.parse(localStorage.getItem('users')) || []
+     const validUser = Users.find(user => user.mail === email && user.password === password)
+     if(!validUser){
+          return Swal.fire({
+               icon: 'error',
+               title: 'Oops...',
+               text: 'Usuario y/o contraseña Incorrecto',
+               timer: 1000
+          })
+     }
+     Swal.fire({
+          icon: 'success',
+          title: 'Felicitaciones',
+          text: 'Bienvenido',
+          timer: 1000
+        })
+     localStorage.setItem('login_success', JSON.stringify(validUser))
+     window.location.href = '../notas.html'
+})   
+
+const user = JSON.parse(localStorage.getItem('login_success')) || false
+if(!user){
+     window.location.href = './index.html'
+}
+
+logout.addEventListener('click', ()=>{
+     Swal.fire({
+          icon: 'success',
+          title: 'Hasta Pronto',
+          text: 'Gracias!',
+          timer: 1000
+        })
+     localStorage.removeItem('login_success')
+     window.location.href = './index.html'
+})
 
 //----------------------------------------------------------//
 eventListeners();
