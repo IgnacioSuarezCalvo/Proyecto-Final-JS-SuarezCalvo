2d4f6d39f6465781a228925a6fd055c99c17fa3a
Ignacio <igf.suarez@gmail.com>
2023-08-10 15:23:08 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -13,11 +13,11 @@ signupForm.addEventListener('submit',(e)=>{
      const isUserRegistered = Users.find(user => user.mail === email)
      if(isUserRegistered){
         return Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Ya esta registrado',
-            timer: 1000
-          })
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Something went wrong!',
+          timer: 1000,
+        })
      }
      Users.push({name: name, email: email, password: password})
      localStorage.setItem('users', JSON.stringify(Users))
@@ -25,9 +25,9 @@ signupForm.addEventListener('submit',(e)=>{
         icon: 'success',
         title: 'Felicitaciones',
         text: 'Ya estas registrados',
-        timer: 1000
+        timer: 1500,
       })
-      window.location.href = '../index.html'
+      window.location.href = './index.html'
 })
 
 loginForm.addEventListener('submit', (e)=>{
@@ -38,18 +38,18 @@ loginForm.addEventListener('submit', (e)=>{
           return Swal.fire({
                icon: 'error',
                title: 'Oops...',
-               text: 'Usuario y/o contraseña Incorrecto',
-               timer: 1000
-          })
+               text: 'Something went wrong!',
+               timer: 1500,
+             })
      }
      Swal.fire({
           icon: 'success',
           title: 'Felicitaciones',
           text: 'Bienvenido',
-          timer: 1000
+          timer: 1000,
         })
      localStorage.setItem('login_success', JSON.stringify(validUser))
-     window.location.href = '../notas.html'
+     window.location.href = './notas.html'
 })   
 
 const user = JSON.parse(localStorage.getItem('login_success')) || false
@@ -62,103 +62,10 @@ logout.addEventListener('click', ()=>{
           icon: 'success',
           title: 'Hasta Pronto',
           text: 'Gracias!',
-          timer: 1000
+          timer: 1000,
         })
      localStorage.removeItem('login_success')
      window.location.href = './index.html'
 })
 
 //----------------------------------------------------------//
-eventListeners();
-
-function eventListeners() {
-     
-     document.querySelector('#formulario').addEventListener('submit', agregarNota);
-     listaNotas.addEventListener('click', borrarNota);
-     document.addEventListener('DOMContentLoaded', localStorageListo); 
-}
-
-function agregarNota(e) {
-     e.preventDefault();
-     
-     const nota = document.getElementById('nota').value;
-     
-     const botonBorrar = document.createElement('a');
-     botonBorrar.classList = 'borrar-nota';
-     botonBorrar.innerText = ' X';
-
-     const li = document.createElement('li');
-     li.innerText = nota;
-     
-     li.appendChild(botonBorrar);
-     
-     listaNotas.appendChild(li);
-
-     agregarNotaLocalStorage(nota);
-}
-
-function borrarNota(e) {
-     e.preventDefault();
-     if(e.target.className === 'borrar-nota') {
-          e.target.parentElement.remove();
-          borrarNotaLocalStorage(e.target.parentElement.innerText);
-     } 
-}
-
-function localStorageListo() {
-     let notas;
-
-     notas = obtenerNotasLocalStorage();
-
-     notas.forEach(function(nota) {
-          
-          const botonBorrar = document.createElement('a');
-          botonBorrar.classList = 'borrar-nota';
-          botonBorrar.innerText = 'X';
-
-          
-          const li = document.createElement('li');
-          li.innerText = nota;
-          
-          li.appendChild(botonBorrar);
-          
-          listaNotas.appendChild(li);
-     });
-}
-
-function obtenerNotasLocalStorage() {
-     let notas;
-     
-     if(localStorage.getItem('notas') === null) {
-          notas = []; 
-     } else {
-          notas = JSON.parse(localStorage.getItem('notas')); 
-     }
-     return notas;
-}
-
-function agregarNotaLocalStorage(nota) {
-     let notas;
-     notas = obtenerNotasLocalStorage();
-     
-     notas.push(nota);
-     
-     localStorage.setItem('notas', JSON.stringify(notas)); 
-}
-
-function borrarNotaLocalStorage(nota) {
-
-     let notas, notaBorrar;
-     
-     notaBorrar = nota.substring(0, nota.length - 1); 
-
-     notas = obtenerNotasLocalStorage();
-
-     notas.forEach(function(nota, index) {
-          if(notaBorrar === nota) {
-               notas.splice(index, 1); 
-          }
-     });
-
-     localStorage.setItem('notas', JSON.stringify(notas)); 
-    }
